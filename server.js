db7f8f84a964c0d7de32d341d1914f0f005f19ee
Qohelet <benjy.shuter@gmail.com>
2024-01-04 06:10:08 -0500

    Adds a redirect to the homepage for any route that isn't specified.

--- server.js
+++ server.js
@@ -15,6 +15,9 @@ app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html
 
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
 
+// app.get('/*', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
+app.get('/*', (req, res) => res.redirect('/'));
+
 app.listen(PORT, () =>
   console.log(`Listening at http://localhost:${PORT}`)
 );
